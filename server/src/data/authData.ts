50f266d04cc57bc9f34d75104ee2bb21ed78f31a
Nickolas Zamachnoi <z.nickolas523@gmail.com>
2024-04-01 16:17:59 -0400

    create donee

--- server/src/data/authData.ts
+++ server/src/data/authData.ts
@@ -1,6 +1,39 @@
-import { RegisterApiRequest } from "../models/io/authIo"
+import { db } from "../lib/db"
+import { RegisterApiRequest, AccountData } from "../models/io/authIo"
 
-export default function createAccount(regDetails: RegisterApiRequest): any {
+export default async function createAccount(
+	regDetails: RegisterApiRequest
+): Promise<AccountData> {
 	const { username, password, first_name, last_name, type } = regDetails
-	return {}
+	const user = await db
+		.insertInto("users")
+		.values({
+			username,
+			password,
+			first_name,
+			last_name,
+			type,
+		})
+		.returningAll()
+		.executeTakeFirstOrThrow()
+
+	if (type === "Member") {
+		await db
+			.insertInto("members")
+			.values({
+				member_id: user.user_id,
+			})
+			.execute()
+	}
+
+	if (type === "Trainer") {
+		await db
+			.insertInto("trainers")
+			.values({
+				trainer_id: user.user_id,
+			})
+			.execute()
+	}
+
+	return user
 }

3df53d40b85e536149a678461fbe6bd405d6b159
Nickolas Zamachnoi <z.nickolas523@gmail.com>
2024-04-03 18:17:00 -0400

    payments done

---
+++ server/src/data/paymentsData.ts
@@ -0,0 +1,38 @@
+import { db } from "../lib/db"
+
+import { PaymentsData } from "../models/io/paymentsIo"
+
+export async function getAllPayments(): Promise<PaymentsData[]> {
+	const payments = await db.selectFrom("payments").selectAll().execute()
+
+	if (!payments) {
+		throw new Error("No payments found")
+	}
+
+	return payments
+}
+
+export async function getPaymentById(id: number): Promise<PaymentsData> {
+	const payment = await db
+		.selectFrom("payments")
+		.where("payment_id", "=", id)
+		.selectAll()
+		.executeTakeFirstOrThrow()
+
+	if (!payment) {
+		throw new Error("No payment found")
+	}
+
+	return payment
+}
+
+export async function processPayment(paymentId: number) {
+	const payment = await db
+		.updateTable("payments")
+		.set("processed", true)
+		.where("payment_id", "=", paymentId)
+		.returningAll()
+		.executeTakeFirstOrThrow()
+
+	return payment
+}

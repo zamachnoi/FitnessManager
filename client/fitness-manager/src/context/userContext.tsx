1fcbcbbb8ee5c3142b32c43877d41c456d95ec72
jmcn99 <jmcnamara700@gmail.com>
2024-04-11 18:00:16 -0400

    small changes

--- client/fitness-manager/src/context/userContext.tsx
+++ client/fitness-manager/src/context/userContext.tsx
@@ -1,71 +1,31 @@
 // UserContext.tsx
-import { getData } from "@/utils/getData"
-import React, {
-	createContext,
-	useContext,
-	useState,
-	ReactNode,
-	FunctionComponent,
-	useEffect,
-} from "react"
+import React, { createContext, useContext, useState, ReactNode, FunctionComponent } from 'react';
 
 type UserContextType = {
-	userId: number | null
-	setUserId: (userId: number | null) => void
-	userType: "Member" | "Trainer" | "Admin" | null
-	setUserType: (userType: "Member" | "Trainer" | "Admin" | null) => void
-}
+  userId: string | null;
+  setUserId: (userId: string | null) => void;
+};
 
 // Define the context with an initial undefined value
-const UserContext = createContext<UserContextType | undefined>(undefined)
+const UserContext = createContext<UserContextType | undefined>(undefined);
 
 type UserProviderProps = {
-	children: ReactNode
-}
+  children: ReactNode;
+};
 
-export const UserProvider: FunctionComponent<UserProviderProps> = ({
-	children,
-}) => {
-	const authEnabled = true
+export const UserProvider: FunctionComponent<UserProviderProps> = ({ children }) => {
+  const [userId, setUserId] = useState<string | null>(null);
 
-	const [userId, setUserId] = useState<number | null>(null)
-	const [userType, setUserType] = useState<
-		"Member" | "Trainer" | "Admin" | null
-	>(null)
+  const value = { userId, setUserId };
 
-	const getSessionData = async () => {
-		const sessionData = await getData("auth/dashboard")
-		if (authEnabled) {
-			console.log(sessionData)
-			if (sessionData.status !== 200) {
-				setUserId(null)
-				setUserType(null)
-				return
-			} else {
-				setUserId(sessionData.data.user_id)
-				setUserType(sessionData.data.type)
-				console.log(userType)
-			}
-		} else {
-			setUserId(1)
-			setUserType("Member")
-		}
-	}
-
-	useEffect(() => {
-		getSessionData()
-	}, [])
-
-	const value = { userId, setUserId, userType, setUserType }
-
-	return <UserContext.Provider value={value}>{children}</UserContext.Provider>
-}
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
 
 // Custom hook to use the context
 export function useUser() {
-	const context = useContext(UserContext)
-	if (context === undefined) {
-		throw new Error("useUser must be used within a UserProvider")
-	}
-	return context
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+  return context;
 }
--- sql/database.bat
+++ sql/database.bat
@@ -1,5 +1,6 @@
-export PGPASSWORD=test
-psql -h localhost -p 15432 -U postgres -d postgres -f setup.sql
+@echo off
+set PGPASSWORD=test
+psql -h localhost -p 15432 -U postgres -d postgres -f ddl.sql
 psql -h localhost -p 15432 -U postgres -d postgres -f dml.sql
 cd ../server
 npx kysely-codegen --out-file ../server/src/models/db/types.d.ts
